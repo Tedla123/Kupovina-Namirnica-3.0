fc40442701814ce6f1491c71e24083c4360dd0be
Tedla123 <todtling@protonmail.com>
2025-04-30 21:11:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -79,14 +79,6 @@ function switchLanguage(lang) {
 }
 
 function renderCategories() {
-
-  catHeader.onclick = () => {
-  document.querySelectorAll(".category").forEach(c => {
-    c.classList.remove("active");
-  });
-  catDiv.classList.add("active");
-};
-
   const container = document.getElementById("categoriesContainer");
   container.innerHTML = "";
 
